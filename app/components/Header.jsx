3465de756a585552e56ad7faa14ccbc51c525dff
Yasas Ramod <yasasramod@gmail.com>
2025-02-10 20:11:22 +0530

    transition done

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -26,14 +26,27 @@ const Header = () => {
         Hi! I'm Yasas Ramod
         <Image src={assets.hand_icon} alt="handIcon" className="w-6" />
       </motion.h3>
-      <h1 className="text-3xl sm:text-6xl lg:text-[66px] font-Ovo">
+      <motion.h1
+        initial={{ y: -30, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.8, delay: 0.5 }}
+        className="text-3xl sm:text-6xl lg:text-[66px] font-Ovo"
+      >
         Passionate Software Engineer from Sri Lanka
-      </h1>
-      <p className="max-w-2xl mx-auto font-Ovo">
+      </motion.h1>
+      <motion.p
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.6, delay: 0.7 }}
+        className="max-w-2xl mx-auto font-Ovo"
+      >
         Transforming ideas into reality with code.
-      </p>
+      </motion.p>
       <div className="flex flex-col sm:flex-row items-center gap-4 mt-4">
-        <a
+        <motion.a
+          initial={{ y: 30, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.6, delay: 1 }}
           href="#contact"
           className="px-10 py-3 border border-white rounded-full bg-black text-white flex item-center gap-2 dark:bg-transparent"
         >
@@ -43,8 +56,11 @@ const Header = () => {
             alt="Contact arrow"
             className="w-4"
           />
-        </a>
-        <a
+        </motion.a>
+        <motion.a
+          initial={{ y: 30, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.6, delay: 1.2 }}
           href="/sample-resume.pdf"
           download
           className="px-10 py-3 border rounded-full border-gray-500 flex item-center gap-2 dark:bg-white dark:text-black"
@@ -55,7 +71,7 @@ const Header = () => {
             alt="Contact arrow"
             className="w-4 h-6"
           />
-        </a>
+        </motion.a>
       </div>
     </div>
   );
--- app/components/Services.jsx
+++ app/components/Services.jsx
@@ -1,19 +1,51 @@
 import { assets, serviceData } from "@/assets/assets";
 import React from "react";
 import Image from "next/image";
+import { motion } from "motion/react";
 
 const Services = () => {
   return (
-    <div id="services" className="w-full px-[12%] py-10 scroll-mt-20">
-      <h4 className="text-center mb-2 text-lg font-Ovo">What I Offer</h4>
-      <h2 className="text-center text-5xl font-Ovo">My services</h2>
-      <p className="text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo">
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+      id="services"
+      className="w-full px-[12%] py-10 scroll-mt-20"
+    >
+      <motion.h4
+        initial={{ opacity: 0, y: -20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: 0.3 }}
+        className="text-center mb-2 text-lg font-Ovo"
+      >
+        What I Offer
+      </motion.h4>
+      <motion.h2
+        initial={{ opacity: 0, y: -20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: 0.5 }}
+        className="text-center text-5xl font-Ovo"
+      >
+        My services
+      </motion.h2>
+      <motion.p
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.5, delay: 0.7 }}
+        className="text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo"
+      >
         I am a developer from Sri Lanka, and I can help you with the following
         services.
-      </p>
-      <div className="grid grid-cols-auto gap-6 my-10">
+      </motion.p>
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.6, delay: 0.9 }}
+        className="grid grid-cols-auto gap-6 my-10"
+      >
         {serviceData.map(({ icon, title, description, link }, index) => (
-          <div
+          <motion.div
+            whileHover={{ scale: 1.05 }}
             key={index}
             className="border border-gray-400 rounded-lg px-8 py-12 cursor-pointer hover:-translate-y-1 duration-500 hover:shadow-black hover:bg-lightHover dark:hover:bg-darkHover dark:hover:shadow-white"
           >
@@ -28,10 +60,10 @@ const Services = () => {
               Read more{" "}
               <Image alt="arrow" src={assets.right_arrow} className="w-4" />
             </a>
-          </div>
+          </motion.div>
         ))}
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
   );
 };
 
